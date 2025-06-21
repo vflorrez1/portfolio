e0d61eaeae9627f56d02e4d12d23587e1fb89e77
vic <manuel1212@hotmail.co.uk>
2025-06-21 21:55:00 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+// import {
+//   About,
+//   Contact,
+//   Experience,
+//   Hero,
+//   Navbar,
+//   StarsCanvas,
+//   Tech,
+//   Works,
+// } from "./components";
+
+const App = () => {
+  return (
+    // <BrowserRouter
+    //   future={{
+    //     v7_startTransition: true,
+    //     v7_relativeSplatPath: true,
+    //   }}
+    // >
+    <div>my new app</div>
+    // </BrowserRouter>
+  );
+};
+
+export default App;
