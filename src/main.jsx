d378b7f266ef6c3ee0bb11d18f42fd63addaad69
vic <manuel1212@hotmail.co.uk>
2025-06-21 22:01:12 +0100

    css broken

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
